86f2392ca437b86d5a22b089c1caee4fe005be67
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-04-22 09:28:21 +0200

    Frame control plugin implemented

--- src/plugins/es.upv.paella.frameControlButtonPlugin.js
+++ src/plugins/es.upv.paella.frameControlButtonPlugin.js
@@ -4,6 +4,8 @@ import {
 } from 'paella-core';
 
 import photoIcon from '../icons/photo.svg';
+import arrowLeftIcon from '../icons/arrow-left.svg';
+import arrowRightIcon from '../icons/arrow-right.svg';
 import '../styles/FrameControlButton.css';
 
 export default class FrameControlButtonPlugin extends PopUpButtonPlugin {
@@ -19,9 +21,6 @@ export default class FrameControlButtonPlugin extends PopUpButtonPlugin {
         this.frames = [
             ...this.frames, 
             ...this.frames, 
-            ...this.frames,
-            ...this.frames,
-            ...this.frames,
             ...this.frames
         ];
         return enabled && this.frames
@@ -29,23 +28,30 @@ export default class FrameControlButtonPlugin extends PopUpButtonPlugin {
 
     async getContent() {
         const content = createElementWithHtmlText('<div class="frame-control-plugin-container"></div>');
-        const leftButton = createElementWithHtmlText(`<button class="btn-left">a</button>`,content);
         const imageContainer = createElementWithHtmlText('<div class="image-list"></div>',content);
-        const rightButton = createElementWithHtmlText(`<button class="btn-right">d</button>`,content);
+        const leftButton = createElementWithHtmlText(`<button class="btn-left"><i class="button-icon">${ arrowLeftIcon }</i></button>`,content);
+        const rightButton = createElementWithHtmlText(`<button class="btn-right"><i class="button-icon">${ arrowRightIcon }</i></button>`,content);
 
         this.frames.forEach(frameData => {
             const frameElement = createElementWithHtmlText(`
-            <a>
-                <img src="${ frameData.thumb }" alt="${ frameData.id }"/>
-            </a>
+                <a><img src="${ frameData.thumb }" alt="${ frameData.id }"/></a>
             `, imageContainer);
             frameElement.__data = frameData;
             frameElement.addEventListener("click", async evt => {
-                console.log(evt.currentTarget.__data);
                 const { time } = evt.currentTarget.__data;
                 await this.player.videoContainer.setCurrentTime(time);
             });
-        })
+        });
+
+        const displacement = () => imageContainer.offsetWidth * 20 / 100;
+        leftButton.addEventListener('click', evt => {
+            imageContainer.scrollLeft -= displacement();
+        });
+
+        rightButton.addEventListener('click', evt => {
+            imageContainer.scrollLeft += displacement();
+        });
+
         return content;
     }
 
