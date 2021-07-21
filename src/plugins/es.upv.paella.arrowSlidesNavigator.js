d495040e90e08ad1fb314bf3a1c905492c0565c3
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-07-21 09:18:50 +0200

    arrow slides navigator styles added

--- src/plugins/es.upv.paella.arrowSlidesNavigator.js
+++ src/plugins/es.upv.paella.arrowSlidesNavigator.js
@@ -1,6 +1,8 @@
 import { Events, EventLogPlugin, createElementWithHtmlText } from 'paella-core';
 
 import "../styles/arrowSlidesNavigator.css";
+import arrowLeft from '../icons/arrow-left.svg';
+import arrowRight from '../icons/arrow-right.svg';
 
 export default class ArrowSlidesNavigatorPlugin extends EventLogPlugin {
     get events() {
@@ -21,17 +23,18 @@ export default class ArrowSlidesNavigatorPlugin extends EventLogPlugin {
 
         if (stream) {
             console.log(`TODO: Implement arrow slides navigator plugin on target "${ target }"`);
+            const mainContainer = createElementWithHtmlText(`<div class="arrow-slides-navigator"></div>`, stream.canvas.userArea);
             const leftButton = createElementWithHtmlText(`
-            <button class="arrow-slides-navigator button-prev">pev</button>
-            `, stream.canvas.userArea);
+            <button class="button-prev"><i>${arrowLeft}</i></button>
+            `, mainContainer);
             leftButton.addEventListener("click", (evt) => {
                 alert("Prev")
                 evt.stopPropagation();
             });
 
             const rightButton = createElementWithHtmlText(`
-            <button class="arrow-slides-navigator button-next">next</button>
-            `, stream.canvas.userArea);
+            <button class="button-next"><i>${arrowRight}</i></button>
+            `, mainContainer);
             rightButton.addEventListener("click", (evt) => {
                 alert("Next");
                 evt.stopPropagation();
